c2a608f929d7c6ba78fa3325ac081ca6293ff6a4
unknown-user <unknown-user@fe80::d01d:df1e:22a9:1e43%11>
2013-07-15 12:54:14 -0400

    Se crea ventana conf Sis

---
+++ confSis.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ControllerGUI
+{
+	public partial class confSis : Gtk.Window
+	{
+		public confSis () : 
+				base(Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+		}
+	}
+}
+
